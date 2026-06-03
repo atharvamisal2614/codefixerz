f62d264091c1bec430f427ef67738e260e03f805
atharva <misalatharva407@gmail.com>
2026-06-03 12:38:00 +0530

    Map updated

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -132,8 +132,17 @@ const ContactSection = () => {
 
             {/* Map Area */}
             <div className="relative w-full h-[200px] sm:h-[250px] rounded-2xl overflow-hidden border border-gray-100 bg-gray-50">
-              <iframe
-                src="https://maps.google.com/maps?q=Ahilyanagar,%20Maharashtra,%20India&t=&z=13&ie=UTF8&iwloc=&output=embed"
+              {/* <iframe
+                src="https://maps.app.goo.gl/L5j9CZiTtUoRj8kz5"
+                width="100%"
+                height="100%"
+                style={{ border: 0 }}
+                allowFullScreen={false}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                className="absolute inset-0"
+              /> */}
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.2232966528227!2d74.73545647466571!3d19.09785755128704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdcb17aff85990f%3A0x39863fab9443a03e!2sCodefixerz!5e0!3m2!1sen!2sin!4v1780470370943!5m2!1sen!2sin" 
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
